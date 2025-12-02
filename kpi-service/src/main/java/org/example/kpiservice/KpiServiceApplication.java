c4be4087f44bf871e3548e07cdaa0bf0b6a4f47d
sanoth-exabyting <sanoth.debnath@exabyting.com>
2025-12-02 17:28:33 +0600

    Initialized project

---
+++ kpi-service/src/main/java/org/example/kpiservice/KpiServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.kpiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpiServiceApplication.class, args);
+    }
+
+}
